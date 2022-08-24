564e8a1c93720736ebee09c0a74e5a1cb888aa03
2seunghye <tmdgp970120@naver.com>
2022-08-24 14:38:54 +0900

    필요한 데이터 모델링

--- src/Component/Todo.tsx
+++ src/Component/Todo.tsx
@@ -1,5 +1,36 @@
 import React from 'react';
 
+type Category = 'sample1' | 'sample2' | 'sample3';
+
+type Tag = {
+  tagId: number;
+  tagText: string;
+};
+
+type State = {
+  id: number;
+  text: string;
+  isDone: boolean;
+  category: Category;
+  tags?: Tag[];
+};
+
+type Action =
+  | { type: 'CREATE'; text: string }
+  | { type: 'READ_ALL' }
+  | { type: 'READ_BY_ID'; id?: number }
+  | { type: 'DELETE'; id?: number }
+  | { type: 'DELETE_ALL' }
+  | {
+      type: 'UPDATE';
+      id: number;
+      text?: string;
+      isDone?: boolean;
+      category?: Category;
+      tagId?: number;
+      tagText?: string;
+    };
+
 const Todo = () => {
   return <div></div>;
 };
